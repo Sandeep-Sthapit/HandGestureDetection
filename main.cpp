c865a3206bc382142ea778302abf9265a0da246d
Sandeep-777 <sandeepsthapit@gmail.com>
2016-07-16 13:29:12 +0545

    Final Commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    Mat img = imread("lena.jpg", CV_LOAD_IMAGE_COLOR);
+    if(img.empty()) 
+       return -1;
+    namedWindow( "lena", CV_WINDOW_AUTOSIZE );
+    imshow("lena", img);
+    waitKey(0);
+    return 0;
+}
+
+
+
